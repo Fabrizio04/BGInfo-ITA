aa32da338ad79d02000259ed4e6cb60b342cb679
Fabrizio04 <amorellif94@gmail.com>
2021-02-13 21:54:00 +0100

    cmd

---
+++ cmd/en.bat
@@ -0,0 +1,5 @@
+@echo off
+set bginfo="C:\Program Files\BGInfo\Bginfo.exe"
+set bgi="C:\Program Files\BGInfo\bgi\en.bgi"
+
+%bginfo% %bgi% /TIMER:0 /nolicprompt
---
+++ cmd/it.bat
@@ -0,0 +1,5 @@
+@echo off
+set bginfo="C:\Program Files\BGInfo\Bginfo.exe"
+set bgi="C:\Program Files\BGInfo\bgi\it.bgi"
+
+%bginfo% %bgi% /TIMER:0 /nolicprompt
